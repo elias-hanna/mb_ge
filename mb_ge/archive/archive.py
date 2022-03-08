06572ec5a40b5c7a6ef5be2a104ba6fb4b212a6b
Elias Hanna <h.elias@hotmail.fr>
2022-03-08 15:56:44 +0100

    modified dump

--- mb_ge/archive/archive.py
+++ mb_ge/archive/archive.py
@@ -1,5 +1,7 @@
 from abc import abstractmethod
 import os
+import numpy as np
+import copy
 
 class Archive():
     def __init__(self, params=None):
@@ -37,6 +39,61 @@ class Archive():
         """
         raise NotImplementedError
 
+    def dump_archive(self, dump_path, budget_used, itr):
+        path_to_dir_to_create = os.path.join(self.dump_path, f'results_{itr}')
+        os.makedirs(path_to_dir_to_create, exist_ok=True)
+        self.visualize(budget_used, itr=itr)
+        total_num_of_els = 0
+        all_max_len_desc = []
+        all_max_len_params = []
+        all_max_len_traj = []
+        for cell in self._archive.values():
+            total_num_of_els += len(cell._elements)
+            len_desc_in_cell = []
+            len_params_in_cell = []
+            len_traj_in_cell = []
+            for el in cell._elements:
+                if el.descriptor is not None and el.policy_parameters is not None \
+                   and el.trajectory is not None:
+                    len_desc_in_cell.append(len(el.descriptor))
+                    len_params_in_cell.append(len(el.policy_parameters))
+                    len_traj_in_cell.append(len(el.trajectory))
+                if len_desc_in_cell!=[] and len_params_in_cell!=[] and len_traj_in_cell!=[]:
+                    all_max_len_desc.append(max(len_desc_in_cell))
+                    all_max_len_params.append(max(len_params_in_cell))
+                    all_max_len_traj.append(max(len_traj_in_cell))
+                    
+        # total_num_of_els = sum([len(cell._elements)
+        # for cell in self.state_archive._archive.values()])
+        # len_desc = len(list(self.state_archive._archive.values())[0]._elements[0].descriptor)
+        # len_params = len(list(self.state_archive._archive.values())[0]._elements[0].policy_parameters)
+        # max_traj_len = max([len(list(self.state_archive._archive.values())[0]._elements)])
+        if all_max_len_desc != [] and all_max_len_params != [] and all_max_len_traj != []:
+            len_desc = max(all_max_len_desc)
+            len_params = max(all_max_len_params)
+            len_traj = max(all_max_len_traj)
+            descriptors = np.zeros((total_num_of_els, len_desc))
+            prev_descriptors = np.zeros((total_num_of_els, len_desc))
+            params = np.zeros((total_num_of_els, len_params))                
+            count = 0
+            for key in self._archive.keys():
+                for el in self._archive[key]._elements:
+                    # import pdb; pdb.set_trace()
+                    descriptors[count, :] = copy.copy(el.descriptor)
+                    if el.previous_element is not None:
+                        prev_descriptors[count, :] = copy.copy(el.previous_element.descriptor)
+                    else:
+                        prev_descriptors[count, :] = copy.copy(el.descriptor)
+                    params[count, :] = copy.copy(el.policy_parameters)
+                    count += 1
+            np.savez(f'{dump_path}/results_{itr}/descriptors', descriptors)
+            np.savez(f'{dump_path}/results_{itr}/prev_descriptors', prev_descriptors)
+            np.savez(f'{dump_path}/results_{itr}/params', params)
+
+                # for key in self.state_archive._archive.keys():
+                    # np.savez_compressed(f'{self.dump_path}/results_{itr}/archive_cell_{key}_itr_{itr}',
+                            # self.state_archive._archive[key]._elements)
+
     def visualize(self, curr_budget, show=False, mode='3d', itr=0):
         import matplotlib.pyplot as plt
         import numpy as np
@@ -56,9 +113,6 @@ class Archive():
 
         ticks = [self._grid_min + i*(self._grid_max - self._grid_min)/self._grid_div
                  for i in range(self._grid_div)]
-        # plt.xticks(ticks)
-        # plt.yticks(ticks)
-        # plt.zticks(ticks)
         ax.set_xticks(ticks)
         ax.set_yticks(ticks)
         ax.set_zticks(ticks)
@@ -83,3 +137,4 @@ class Archive():
         plt.savefig(f"{self.dump_path}/results_{itr}/state_archive_at_{curr_budget}_eval", bbox_inches='tight')
         if show:
             plt.show()
+        plt.close()
