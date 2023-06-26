5c9ffe88368269f083459080a88f0d9047806c97
Elias Hanna <h.elias@hotmail.fr>
2023-06-26 11:24:29 +0200

    added a check of gym version so that goalenv is not declared if the gym version doesnt support goalenvs (as of gym 0.21>=)

--- mb_ge/envs/__init__.py
+++ mb_ge/envs/__init__.py
@@ -1,3 +1,6 @@
 from mb_ge.envs.ball_in_cup_env import BallInCupEnv
 from mb_ge.envs.ball_in_cup_3d_env import BallInCup3dEnv
-from mb_ge.envs.ball_in_cup_3d_goal_env import BallInCup3dGoalEnv
+import gym
+major, minor, patch = gym.__version__.split('.')
+if minor > 9 and minor < 21:
+    from mb_ge.envs.ball_in_cup_3d_goal_env import BallInCup3dGoalEnv
